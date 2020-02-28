849fca0fd1160087affbf87c48095648f6c578b0
Angie DePasquale <angied521@yahoo.com>
2020-02-28 11:36:24 -0500

    route post added needs api axios

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get('/api/user_data').then(function (data) {
-    $('.member-name').text(data.email)
+
+  /// create an on click for submit button
+
+  // then execute this
+  $.get('/api/parks').then(function (data) {
+   console.log(data)
   })
 })
