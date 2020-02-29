7dd5b3d9d83911d08b14453296cf187951b1cba1
Athagod <afordhal10@mail.bw.edu>
2020-02-28 20:12:35 -0500

    working on onclick

--- public/js/members.js
+++ public/js/members.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
   // and updates the HTML on the page
 
   /// create an on click for submit button
+$("submit").on("click", function(){
+    var parkOptions = {
+      camper: $("#").val()
+    }
+//// on click not complete
+
+
+    
+})
 
   // then execute this
   $.get('/api/parks').then(function (data) {
